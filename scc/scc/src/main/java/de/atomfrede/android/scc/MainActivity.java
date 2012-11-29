f3c9db3cee90ea061262aba5a0bebc95415d4d1a
fred <fred@endor>
2012-11-29 20:58:53 +0100

    Updated layouts and pom.

--- scc/scc/src/main/java/de/atomfrede/android/scc/MainActivity.java
+++ scc/scc/src/main/java/de/atomfrede/android/scc/MainActivity.java
@@ -4,25 +4,15 @@ import com.googlecode.androidannotations.annotations.EActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
-@EActivity
-public class MainActivity extends Activity {
+@EActivity(R.layout.activity_main)
+public class MainActivity extends FragmentActivity {
 
     private static String TAG = "scc";
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
-    }
+   
 
 }
 
