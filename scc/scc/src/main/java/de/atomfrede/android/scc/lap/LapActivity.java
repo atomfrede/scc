4bccf8dce78aa57fe1b4cf01fb5a216f4cd02cdc
fred <fred@endor>
2012-12-05 08:37:59 +0100

    Emptry skeletons for displaying the laps of a competition.

---
+++ scc/scc/src/main/java/de/atomfrede/android/scc/lap/LapActivity.java
@@ -0,0 +1,10 @@
+package de.atomfrede.android.scc.lap;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+
+import android.support.v4.app.FragmentActivity;
+
+@EActivity
+public class LapActivity extends FragmentActivity {
+
+}
---
+++ scc/scc/src/main/java/de/atomfrede/android/scc/lap/LapFragment.java
@@ -0,0 +1,10 @@
+package de.atomfrede.android.scc.lap;
+
+import com.googlecode.androidannotations.annotations.EFragment;
+
+import android.support.v4.app.Fragment;
+
+@EFragment
+public class LapFragment extends Fragment {
+
+}
