6fdd3cb41a9270df3fb7b12af57b2b5cd9defa61
fred <fred@endor>
2012-12-05 08:47:19 +0100

    Added Viewpager Indicator to eclipse configuration.

--- scc/scc/src/main/java/de/atomfrede/android/scc/lap/LapFragment.java
+++ scc/scc/src/main/java/de/atomfrede/android/scc/lap/LapFragment.java
@@ -1,10 +1,20 @@
 package de.atomfrede.android.scc.lap;
 
 import com.googlecode.androidannotations.annotations.EFragment;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.viewpagerindicator.TitlePageIndicator;
+
+import de.atomfrede.android.scc.R;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 
 @EFragment
 public class LapFragment extends Fragment {
 
+	@ViewById(R.id.pager)
+	ViewPager mPager;
+	
+	@ViewById(R.id.titles)
+	TitlePageIndicator mIndicator;
 }
